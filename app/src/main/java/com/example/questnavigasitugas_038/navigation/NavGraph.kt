953f38a275a953fa88dedd9cc050bd7df4cd22ed
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-10 12:42:14 +0700

    Buat sealed class Screen dan Buat kerangka AppNavigation dengan NavHost

--- app/src/main/java/com/example/questnavigasitugas_038/navigation/NavGraph.kt
+++ app/src/main/java/com/example/questnavigasitugas_038/navigation/NavGraph.kt
@@ -1,2 +1,38 @@
 package com.example.questnavigasitugas_038.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasitugas_038.screens.FormScreen
+import com.example.questnavigasitugas_038.screens.ListScreen
+import com.example.questnavigasitugas_038.screens.SplashScreen
+import com.example.questnavigasitugas_038.viewmodel.MainViewModel
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash")
+    object List : Screen("list")
+    object Form : Screen("form")
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val viewModel: MainViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Splash.route
+    ) {
+        composable(Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController = navController, viewModel = viewModel)
+        }
+        composable(Screen.Form.route) {
+            FormScreen(navController = navController, viewModel = viewModel)
+        }
+    }
+}
