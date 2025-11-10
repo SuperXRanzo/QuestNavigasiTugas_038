904f8eb7ec0416f3b024b79f8882424fe1bcdfd5
SuperXRanzo <okanskyavenger@gmail.com>
2025-11-10 12:35:54 +0700

    Buat DataSource dengan data hardcoded

--- app/src/main/java/com/example/questnavigasitugas_038/data/DataSource.kt
+++ app/src/main/java/com/example/questnavigasitugas_038/data/DataSource.kt
@@ -1,2 +1,30 @@
 package com.example.questnavigasitugas_038.data
 
+object DataSource {
+    val participants = listOf(
+        FormData(
+            nama = "Adolf Kitler",
+            gender = "Laki - Laki",
+            status = "Menikah",
+            alamat = "Madiun"
+        ),
+        FormData(
+            nama = "Reza Kecap",
+            gender = "Laki - Laki",
+            status = "Lajang",
+            alamat = "Ngawi"
+        ),
+        FormData(
+            nama = "Siti Kompas",
+            gender = "Perempuan",
+            status = "Kawin",
+            alamat = "Sukabumi"
+        ),
+        FormData(
+            nama = "Yola Octaviano Lukito",
+            gender = "Laki - Laki",
+            status = "Lajang",
+            alamat = "Jogja"
+        )
+    )
+}
